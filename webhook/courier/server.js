f2ac178abe68b137c0275b47c8e99afd8837e224
coffeeT4ble <neilmalhotra228@gmail.com>
2025-11-12 17:59:38 +0100

    Implement order processing simulation
    Added simulated order processing with delays for preparation, delivery, and completion.

--- webhook/courier/server.js
+++ webhook/courier/server.js
@@ -6,7 +6,22 @@ app.use(express.json());
 app.post('/order', (req, res) => {
 	const order = req.body;
 	console.log('BOMBOCLAT', order);
-	res.sendStatus(202);
+	setTimeout(() => {
+    		console.log(`${order.id}: Restaurace připravuje`);
+
+    		setTimeout(() => {
+      			console.log(`${order.id}: Rozváží se`);
+
+      			setTimeout(() => {
+        				console.log(`${order.id}: Doručeno`);
+
+        				res.status(200).json({ id: order.id, status: 'Doručeno' });
+
+      			}, 5000);
+
+    		}, 5000);
+
+  	}, 5000);
 });
 
 app.listen(3000, () => {
